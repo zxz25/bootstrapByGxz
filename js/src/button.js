syf2958389553/my

WIP Prefix our data keys with `bs-`

js/src/button.js
@@ -26,8 +26,8 @@ const CLASS_NAME_ACTIVE = 'active'
 const CLASS_NAME_DISABLED = 'disabled'
 const CLASS_NAME_FOCUS = 'focus'
 
-const SELECTOR_DATA_TOGGLE_CARROT = '[data-toggle^="button"]'
-const SELECTOR_DATA_TOGGLE = '[data-toggle="buttons"]'
+const SELECTOR_DATA_TOGGLE_CARROT = '[data-bs-toggle^="button"]'
+const SELECTOR_DATA_TOGGLE = '[data-bs-toggle="buttons"]'
 const SELECTOR_INPUT = 'input:not([type="hidden"])'
 const SELECTOR_ACTIVE = '.active'
 const SELECTOR_BUTTON = '.btn'

zxz25/bootstrapByGxz

js/src/button.js
@@ -23,7 +23,7 @@ const DATA_API_KEY = '.data-api'
 
 const CLASS_NAME_ACTIVE = 'active'
 
-const SELECTOR_DATA_TOGGLE = '[data-toggle="button"]'
+const SELECTOR_DATA_TOGGLE = '[data-bs-toggle="button"]'
 
 const EVENT_CLICK_DATA_API = `click${EVENT_KEY}${DATA_API_KEY}`
 