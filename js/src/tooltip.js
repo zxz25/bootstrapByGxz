NSDGB/bootstrap

Switch to `Set#has()`

js/src/tooltip.js
@@ -38,7 +38,7 @@ const DATA_KEY = 'bs.tooltip'
 const EVENT_KEY = `.${DATA_KEY}`
 const CLASS_PREFIX = 'bs-tooltip'
 const BSCLS_PREFIX_REGEX = new RegExp(`(^|\\s)${CLASS_PREFIX}\\S+`, 'g')
-const DISALLOWED_ATTRIBUTES = ['sanitize', 'whiteList', 'sanitizeFn']
+const DISALLOWED_ATTRIBUTES = new Set(['sanitize', 'whiteList', 'sanitizeFn'])
 
 const DefaultType = {
   animation: 'boolean',
@@ -678,7 +678,7 @@ class Tooltip {
 
     Object.keys(dataAttributes)
       .forEach(dataAttr => {
-        if (DISALLOWED_ATTRIBUTES.includes(dataAttr)) {
+        if (DISALLOWED_ATTRIBUTES.has(dataAttr)) {
           delete dataAttributes[dataAttr]
         }
       })

zxz25/bootstrapByGxz

js/src/tooltip.js
@@ -38,7 +38,7 @@ const DATA_KEY = 'bs.tooltip'
 const EVENT_KEY = `.${DATA_KEY}`
 const CLASS_PREFIX = 'bs-tooltip'
 const BSCLS_PREFIX_REGEX = new RegExp(`(^|\\s)${CLASS_PREFIX}\\S+`, 'g')
-const DISALLOWED_ATTRIBUTES = ['sanitize', 'allowList', 'sanitizeFn']
+const DISALLOWED_ATTRIBUTES = new Set(['sanitize', 'allowList', 'sanitizeFn'])
 
 const DefaultType = {
   animation: 'boolean',
@@ -678,7 +678,7 @@ class Tooltip {
 
     Object.keys(dataAttributes)
       .forEach(dataAttr => {
-        if (DISALLOWED_ATTRIBUTES.includes(dataAttr)) {
+        if (DISALLOWED_ATTRIBUTES.has(dataAttr)) {
           delete dataAttributes[dataAttr]
         }
       })